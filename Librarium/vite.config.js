947cdd2af2573deb071df75fe6eadadc87190b2d
Michael Burdick <asylumrunner@gmail.com>
2024-11-27 14:34:29 -0600

    Dockerize

--- Librarium/vite.config.js
+++ Librarium/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-  /* server: {
+  plugins: [react()],
+  server: {
     watch: {
       usePolling: true,
     },
-    host: true, // needed for the Docker Container port mapping to work
+    host: true,
     strictPort: true,
-    port: 5173, // you can replace this port with any port
-  } */
+    port: 8080,
+  }
 })
 
