82830d11c74465ef7ba831e5e2614434583956f2
Michael Burdick <asylumrunner@gmail.com>
2025-01-11 20:31:09 -0800

    add basic cognito integration

--- Librarium/src/App.jsx
+++ Librarium/src/App.jsx
@@ -3,18 +3,50 @@ import GridView from './components/GridView'
 import Toolbar from './components/Toolbar'
 import SearchPage from './components/search/SearchPage'
 import { useSelector } from 'react-redux'
+import { useAuth } from "react-oidc-context";
 
 function App() {
+  const auth = useAuth();
+  const returnUrl = "/dashboard"
   const { selectedItem } = useSelector((state) => {
     return state.currentItem
   })
 
-  return (
-    <>
-      <div className="flex flex-col w-screen">
+  const signOutRedirect = () => {
+    const clientId = "6motri9tdloufgqk81niar4rru";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+        <div className="flex flex-col w-screen">
           {selectedItem == null ? <GridView />: <DetailsView item={selectedItem}/>}
+        </div>
+        <button onClick={() => auth.removeUser()}>Sign out</button>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
   )
 }
 
--- Librarium/src/main.jsx
+++ Librarium/src/main.jsx
@@ -4,9 +4,23 @@ import App from './App.jsx'
 import { store } from './store'
 import { Provider } from 'react-redux'
 import './index.css'
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_9B6c16qt6",
+  client_id: "6motri9tdloufgqk81niar4rru",
+  redirect_uri: "http://localhost:8080",
+  response_type: "code",
+  scope: "phone openid email",
+};
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <AuthProvider {...cognitoAuthConfig}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
+  </React.StrictMode>
 )
